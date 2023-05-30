6718ea9b52b67f42d2bb11e3d1826356bfcfd1b1
Skenvy <>
2023-05-30 23:03:45 +1000

    repath app src

---
+++ Sudoku/application/src/test/java/io/github/skenvy/AppTest.java
@@ -0,0 +1,24 @@
+package io.github.skenvy;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue(true);
+        assertFalse(false);
+        assertEquals(1, 1);
+    }
+}
