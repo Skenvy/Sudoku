48ba9d373c3dd9d838785e07dbe4f91997b2a441
Skenvy <17214791+Skenvy@users.noreply.github.com>
2023-06-07 01:12:51 +1000

    remove old scripts

--- scripts/build.bat
+++
@@ -1,7 +0,0 @@
-REM Run as .\scripts\build.bat from the repository root.
-cd sudoku
-call mvn clean package
-FOR /F "delims=" %%i IN ('mvn help:evaluate -Dexpression^="project.version" -q -DforceStdout') DO set VER=%%i
-cd ../sudoku-gui
-call mvn clean package -P target-adjacent-local-build -Delicious.version=%VER%
-cd ..
--- scripts/run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euxo pipefail
-
-# Run as './scripts/run.sh' from the repository root.
-make -C sudoku-gui run
